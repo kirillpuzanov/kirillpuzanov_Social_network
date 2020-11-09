79f6875ba9a0fcd790ed931bb08bdadea8e2b764
kirillpuzanov <kirillpuzanov@mail.ru>
2020-11-09 09:47:47 +0300

    91 (chrome extension / reduxDev tools)

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,11 +1,10 @@
-
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {profileReducer} from "./profile-reducer";
-import {usersReducer} from "./users-reducer";
-import {dialogsReducer} from "./dialogs-reducer";
-import {authReducer} from "./auth-reducer";
-import thunkMiddleware from'redux-thunk';
-import { reducer as formReducer } from 'redux-form'
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import {profileReducer} from './profile-reducer';
+import {usersReducer} from './users-reducer';
+import {dialogsReducer} from './dialogs-reducer';
+import {authReducer} from './auth-reducer';
+import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form'
 import {appReducer} from './app-reducer';
 
 
@@ -16,19 +15,22 @@ type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never
 export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesTypes<T>>
 
 
-
 let reducers = combineReducers({
-    profilePage:profileReducer,
-    messagesPage:dialogsReducer,
-    usersPage:usersReducer,
+    profilePage: profileReducer,
+    messagesPage: dialogsReducer,
+    usersPage: usersReducer,
     auth: authReducer,
     form: formReducer,
     app: appReducer,
     // sideBar:sideBarReducer
 });
-
-
-export let store = createStore(reducers,applyMiddleware(thunkMiddleware));
 // @ts-ignore
-window.store = store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
+
+// @ts-ignore
+window._store_ = store
+
+
+// export let store = createStore(reducers,applyMiddleware(thunkMiddleware));
